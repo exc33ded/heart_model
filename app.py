703292b86dac56d7ef3f1d9188ac35935a5728bc
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2022-08-23 18:30:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,45 @@
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+# Importing essential libraries
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-# Define Flask App
+filename = 'final_model.pkl'
+model_heart = pickle.load(open(filename, 'rb'))
+
 app = Flask(__name__)
 
-# -------------------------Deploying of Front Page----------------------------
+# Rendering main page
 @app.route('/')
-def front_page():
+def home():
+	return render_template('frontpage.html')
+
+@app.route('/heart')
+def heart_page():
     return render_template('heart.html')
 
-model_heart = pickle.load(open('final_model.pkl', 'rb'))
-@app.route('/heart',methods=['POST','GET'])
-def heart_page():
-    if request.method == 'GET':
-        return render_template('heart.html')
-    else:
-        age = request.form['age']
+@app.route('/heartPredict', methods=['GET','POST'])
+def Heart_predict():
+    if request.method == 'POST':
+
+        age = int(request.form['age'])
         sex = request.form.get('sex')
-        cp = request.form['cp'] 
-        tresrbps = request.form['trestbps']
-        chol = request.form['chol']
-        fbs = request.form['fbs'] 
-        restecg = request.form['restecg'] 
-        thalach = request.form['thalach']
-        exang = request.form['exang'] 
-        oldpeak = request.form['oldpeak'] 
-        slope = request.form['slope'] 
-        ca = request.form['ca'] 
-        thal= request.form['thal']
-
-    input_data = (age, sex, cp, tresrbps, chol, fbs, restecg, thalach,
-                    exang, oldpeak, slope, ca, thal)
-    print(input_data)
-    input_data_as_numpy_array= np.asarray(input_data)
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-    prediction = model_heart.predict(input_data_reshaped)
-    senddata=""
-    if (prediction[0]== 0):
-        senddata='According to the given details person does not have any Heart Diesease.'
-    else:
-        senddata='According to the given details chances of having Heart Diesease are High, So Please Consult a Doctor'
-    return render_template('heart.html', prediction_text=senddata)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        cp = request.form.get('cp')
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+        oldpeak = float(request.form['oldpeak'])
+        slope = request.form.get('slope')
+        ca = int(request.form['ca'])
+        thal = request.form.get('thal')
+        
+        data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        my_prediction = model_heart.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+        
+        
+if __name__ == '__main__':
+	app.run(debug=True)
